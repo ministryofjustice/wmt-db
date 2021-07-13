232de823437c18b12f4c130c656ff5c0cf60403a
carlov20 <carlov20@users.noreply.github.com>
2021-07-13 15:25:00 +0100

    WFP-219 added in new views for postgres

---
+++ src/main/resources/db/migration_postgresql/V1_3__SEED_MANAGER_TYPE.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO app.offender_manager_type(id,grade_code,description)
+OVERRIDING SYSTEM  VALUE
+VALUES
+(1,'PO','Probation Officer'),
+(2,'SPO','Senior Probation Officer'),
+(3,'PSO','Probation Support Officer'),
+(4,'TPO','Trainee Probation Officer'),
+(5,'DMY','Dummy Grade'),
+(6,'',NULL);
