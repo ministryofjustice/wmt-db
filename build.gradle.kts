261c7a5de19e09bbd20ff6a992e7a66cf1a22c93
carlov20 <carlov20@users.noreply.github.com>
2021-07-14 13:53:24 +0100

    WFP-219 converted to postgres only

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,7 @@ dependencies {
 
   runtimeOnly("com.zaxxer:HikariCP:3.4.5")
   runtimeOnly("org.flywaydb:flyway-core:6.5.6")
-  runtimeOnly("com.microsoft.sqlserver:mssql-jdbc:9.2.1.jre15")
   runtimeOnly("org.postgresql:postgresql")
-  runtimeOnly("com.h2database:h2:1.4.200")
 }
 
 tasks {
