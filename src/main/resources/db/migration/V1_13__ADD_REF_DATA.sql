b3610b61151586d8a74a1a3241627757fd196ad1
carlov20 <carlov20@users.noreply.github.com>
2021-09-15 16:34:56 +0100

    added more ref data

---
+++ src/main/resources/db/migration/V1_13__ADD_REF_DATA.sql
@@ -0,0 +1,28 @@
+INSERT INTO app.case_category (category_id,category_name) VALUES
+	 (0,'Untiered'),
+	 (1,'A3'),
+	 (2,'A2'),
+	 (3,'A1'),
+	 (4,'A0'),
+	 (5,'B3'),
+	 (6,'B2'),
+	 (7,'B1'),
+	 (8,'B0'),
+	 (9,'C3'),
+	 (10,'C2'),
+	 (11,'C1'),
+	 (12,'C0'),
+	 (13,'D3'),
+	 (14,'D2'),
+	 (15,'D1'),
+	 (16,'D0');
+
+INSERT INTO app.row_type_definitions (row_type,row_type_full_name) VALUES
+	 ('L','Suspended Lifer'),
+	 ('S','Suspended Sentence Order'),
+	 ('P','Persistent Prolific Offender'),
+	 ('I','Integrated Offender Management'),
+	 ('O','Overdue Termination'),
+	 ('U','Unpaid Work'),
+	 ('W','Active Warrant'),
+	 ('N','Normal');
