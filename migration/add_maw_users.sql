631022292cad5a2cff22ea4905ef4a78c488ebd5
carlov20 <carlov20@users.noreply.github.com>
2021-10-27 09:17:22 +0100

    removed duplicate user

--- migration/add_maw_users.sql
+++ migration/add_maw_users.sql
@@ -1,9 +1,7 @@
 INSERT INTO app.users(username, name) values ('adam.lerner', 'Adam Lerner MAW team'),
 ('peter.robinson', 'Peter Robinson MAW team'),
-('mark.rees', 'Mark Rees MAW team'),
-('liam.mcmurray', 'Liam Mcmurray MAW team');
+('mark.rees', 'Mark Rees MAW team');
 
 INSERT into app.user_role(user_id, role_id, last_updated, last_updated_by) values ((select id from app.users where username = 'adam.lerner'), 3, current_timestamp, 'AUTOMATED'),
 ((select id from app.users where username = 'peter.robinson'), 3, current_timestamp, 'AUTOMATED'),
-((select id from app.users where username = 'mark.rees'), 3, current_timestamp, 'AUTOMATED'),
-((select id from app.users where username = 'liam.mcmurray'), 3, current_timestamp, 'AUTOMATED');
+((select id from app.users where username = 'mark.rees'), 3, current_timestamp, 'AUTOMATED');
