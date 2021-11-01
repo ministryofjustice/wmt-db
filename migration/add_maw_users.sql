800a1271978f6179b6d91a5c0383b3c346cbba88
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2021-11-01 13:51:23 +0000

    Add admin user

--- migration/add_maw_users.sql
+++ migration/add_maw_users.sql
@@ -1,7 +1,9 @@
 INSERT INTO app.users(username, name) values ('adam.lerner', 'Adam Lerner MAW team'),
 ('peter.robinson', 'Peter Robinson MAW team'),
-('mark.rees', 'Mark Rees MAW team');
+('mark.rees', 'Mark Rees MAW team'),
+('katie.cawthorne', 'Katie Cawthorne MAW team');
 
 INSERT into app.user_role(user_id, role_id, last_updated, last_updated_by) values ((select id from app.users where username = 'adam.lerner'), 3, current_timestamp, 'AUTOMATED'),
 ((select id from app.users where username = 'peter.robinson'), 3, current_timestamp, 'AUTOMATED'),
-((select id from app.users where username = 'mark.rees'), 3, current_timestamp, 'AUTOMATED');
+((select id from app.users where username = 'mark.rees'), 3, current_timestamp, 'AUTOMATED'),
+((select id from app.users where username = 'katie.cawthorne'), 3, current_timestamp, 'AUTOMATED');
