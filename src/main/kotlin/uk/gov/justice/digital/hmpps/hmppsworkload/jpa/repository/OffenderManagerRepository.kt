786ca234a9a92b42782e6e329e3001a2db927748
carlov20 <carlov20@users.noreply.github.com>
2022-02-11 15:59:39 +0000

    WFP-923 corrected repository

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/OffenderManagerRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/OffenderManagerRepository.kt
@@ -3,11 +3,11 @@ package uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.TeamEntity
+import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.OffenderManagerEntity
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.mapping.OffenderManagerOverview
 
 @Repository
-interface OffenderManagerRepository : CrudRepository<TeamEntity, Long> {
+interface OffenderManagerRepository : CrudRepository<OffenderManagerEntity, Long> {
   @Query(nativeQuery = true)
   fun findByOverview(teamCode: String, offenderManagerCode: String): OffenderManagerOverview?
 }
