c5faafdecb57b9fe8def7c00bc417bb063db0cf5
carlov20 <carlov20@users.noreply.github.com>
2022-02-22 12:59:17 +0000

    WFP-930 updated reduction archive view to have team name

--- src/main/resources/db/migration/V1_23__ADD_TEAM_NAME_ARCHIVE_REDUCTIONS.sql
+++ src/main/resources/db/migration/V1_23__ADD_TEAM_NAME_ARCHIVE_REDUCTIONS.sql
@@ -1,8 +1,8 @@
 
 drop view app.reductions_archive_view;
-CREATE OR REPLACE VIEW app.reductions_archive_view (om_name, hours_reduced, reduction_id, comments, last_updated_date, reduction_added_by, reduction_reason, start_date, end_date, reduction_status, team_name) AS
+CREATE OR REPLACE VIEW app.reductions_archive_view (om_name, hours_reduced, reduction_id, comments, last_updated_date, reduction_added_by, reduction_reason, start_date, end_date, reduction_status, teamName) AS
 SELECT
-    CONCAT(om.forename, ' ', om.surname) AS om_name, r.hours AS hours_reduced, r.id AS reduction_id, r.notes AS comments, r.updated_date AS last_updated_date, u.name AS reduction_added_by, rr.reason_short_name AS reduction_reason, r.effective_from AS start_date, r.effective_to AS end_date, r.status AS reduction_status, t.description AS team_name
+    CONCAT(om.forename, ' ', om.surname) AS om_name, r.hours AS hours_reduced, r.id AS reduction_id, r.notes AS comments, r.updated_date AS last_updated_date, u.name AS reduction_added_by, rr.reason_short_name AS reduction_reason, r.effective_from AS start_date, r.effective_to AS end_date, r.status AS reduction_status, t.description AS teamName
     FROM app.workload_owner AS wo
     JOIN app.offender_manager AS om
         ON om.id = wo.offender_manager_id
