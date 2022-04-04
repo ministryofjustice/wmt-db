1817c6a5d993fe2c87aa62c8b532e1809f2dc35f
carlov20 <carlov20@users.noreply.github.com>
2022-04-04 17:02:38 +0100

    Wfp 917 (#58)
    * WFP-917 made publishing to topic optional depending on whether the topic is configured
    
    * WFP-917 made publishing to topic optional depending on whether the topic is configured

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/LogSuccessUpdater.kt
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.hmpps.hmppsworkload.service
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Conditional
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsworkload.config.NoAllocationCompleteTopicCondition
+import java.time.ZonedDateTime
+import java.util.UUID
+
+@Service
+@Conditional(NoAllocationCompleteTopicCondition::class)
+class LogSuccessUpdater : SuccessUpdater {
+
+  override fun updatePerson(crn: String, allocationId: UUID, timeUpdated: ZonedDateTime) {
+    log.info("crn {} with allocationId {} updated at {}", crn, allocationId, timeUpdated)
+  }
+
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/SqsSuccessUpdater.kt
@@ -0,0 +1,52 @@
+package uk.gov.justice.digital.hmpps.hmppsworkload.service
+
+import com.amazonaws.services.sns.model.MessageAttributeValue
+import com.amazonaws.services.sns.model.PublishRequest
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsworkload.domain.event.HmppsMessage
+import uk.gov.justice.digital.hmpps.hmppsworkload.domain.event.HmppsPersonAllocationMessage
+import uk.gov.justice.digital.hmpps.hmppsworkload.domain.event.PersonReference
+import uk.gov.justice.digital.hmpps.hmppsworkload.domain.event.PersonReferenceType
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.MissingTopicException
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.UUID
+
+@Service
+@ConditionalOnProperty("hmpps.sqs.topics.hmppsallocationcompletetopic.arn")
+class SqsSuccessUpdater(
+  val hmppsQueueService: HmppsQueueService,
+  val objectMapper: ObjectMapper
+) : SuccessUpdater {
+
+  private val allocationCompleteTopic by lazy {
+    hmppsQueueService.findByTopicId("hmppsallocationcompletetopic")
+      ?: throw MissingTopicException("hmppsallocationcompletetopic not found")
+  }
+
+  override fun updatePerson(crn: String, allocationId: UUID, timeUpdated: ZonedDateTime) {
+    val hmppsPersonEvent = HmppsMessage(
+      "PERSON_MANAGER_ALLOCATED", 1, "Person allocated event", "http://dummy.com",
+      timeUpdated.format(
+        DateTimeFormatter.ISO_OFFSET_DATE_TIME
+      ),
+      HmppsPersonAllocationMessage(allocationId),
+      PersonReference(listOf(PersonReferenceType("CRN", crn)))
+    )
+    allocationCompleteTopic.snsClient.publish(
+      PublishRequest(allocationCompleteTopic.arn, objectMapper.writeValueAsString(hmppsPersonEvent))
+        .withMessageAttributes(mapOf("eventType" to MessageAttributeValue().withDataType("String").withStringValue(hmppsPersonEvent.eventType)))
+    ).also {
+      log.info("Published event {} to topic for CRN {} and id {}", hmppsPersonEvent.eventType, crn, allocationId)
+    }
+  }
+
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+}
