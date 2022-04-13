87fd61ef222374218b1e0ff6d81c7ee3844a3512
carlov20 <carlov20@users.noreply.github.com>
2022-04-13 11:05:23 +0100

    WFP-996 fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/controller/StaffController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/controller/StaffController.kt
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsworkload.domain.StaffSummary
@@ -26,7 +26,7 @@ class StaffController(private val getStaffService: GetStaffService) {
     ]
   )
   @PreAuthorize("hasRole('ROLE_WORKLOAD_MEASUREMENT') or hasRole('ROLE_WORKLOAD_READ')")
-  @PostMapping("/staff/{staffId}")
+  @GetMapping("/staff/{staffId}")
   fun getStaffById(@PathVariable(required = true) staffId: BigInteger): StaffSummary =
     getStaffService.getStaffById(staffId)?.let { staff -> StaffSummary.from(staff) } ?: run {
       throw EntityNotFoundException("staff ID not found for $staffId")
