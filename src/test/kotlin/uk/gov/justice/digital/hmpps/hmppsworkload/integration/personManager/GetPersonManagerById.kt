fa3cde6722a3d9dc67414948dd738cf78ee1c77a
carlov20 <carlov20@users.noreply.github.com>
2022-04-13 16:05:24 +0100

    WFP-1023 time comparator fix

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/personManager/GetPersonManagerById.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/personManager/GetPersonManagerById.kt
@@ -1,10 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsworkload.integration.personManager
 
-import org.hamcrest.number.OrderingComparison
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.hmppsworkload.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.PersonManagerEntity
 import java.math.BigInteger
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 class GetPersonManagerById : IntegrationTestBase() {
 
@@ -35,8 +36,10 @@ class GetPersonManagerById : IntegrationTestBase() {
       .jsonPath("$.createdBy")
       .isEqualTo(storedPersonManager.createdBy)
       .jsonPath("$.createdDate")
-      .value(
-        OrderingComparison.comparesEqualTo(storedPersonManager.createdDate!!)
+      .isEqualTo(
+        storedPersonManager.createdDate!!.withZoneSameInstant(ZoneOffset.UTC).format(
+          DateTimeFormatter.ISO_OFFSET_DATE_TIME
+        )
       )
   }
 }
