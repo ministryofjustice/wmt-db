ec536e07baa973e44947e9f7c6dd194b09d425bf
carlov20 <carlov20@users.noreply.github.com>
2022-04-13 14:54:45 +0100

    WFP-1023 added get person manager allocation by id endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/PersonManagerRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/PersonManagerRepository.kt
@@ -2,7 +2,9 @@ package uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository
 
 import org.springframework.data.repository.CrudRepository
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.PersonManagerEntity
+import java.util.UUID
 
 interface PersonManagerRepository : CrudRepository<PersonManagerEntity, Long> {
   fun findFirstByCrnOrderByCreatedDateDesc(crn: String): PersonManagerEntity?
+  fun findByUuid(id: UUID): PersonManagerEntity?
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/personManager/GetPersonManagerById.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.hmppsworkload.integration.personManager
+
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppsworkload.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.PersonManagerEntity
+import java.math.BigInteger
+import java.time.format.DateTimeFormatter
+
+class GetPersonManagerById : IntegrationTestBase() {
+
+  @Test
+  fun `can get person manager by Id`() {
+    val storedPersonManager = PersonManagerEntity(crn = "CRN1", staffId = BigInteger.valueOf(123456789L), staffCode = "OM1", teamCode = "T1", offenderName = "John Doe", createdBy = "USER1", providerCode = "PV1")
+    personManagerRepository.save(storedPersonManager)
+
+    webTestClient.get()
+      .uri("/allocation/person/${storedPersonManager.uuid}")
+      .headers {
+        it.authToken(roles = listOf("ROLE_WORKLOAD_READ"))
+      }
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("$.id")
+      .isEqualTo(storedPersonManager.uuid.toString())
+      .jsonPath("$.staffId")
+      .isEqualTo(storedPersonManager.staffId)
+      .jsonPath("$.staffCode")
+      .isEqualTo(storedPersonManager.staffCode)
+      .jsonPath("$.teamCode")
+      .isEqualTo(storedPersonManager.teamCode)
+      .jsonPath("$.providerCode")
+      .isEqualTo(storedPersonManager.providerCode)
+      .jsonPath("$.createdBy")
+      .isEqualTo(storedPersonManager.createdBy)
+      .jsonPath("$.createdDate")
+      .isEqualTo(
+        storedPersonManager.createdDate!!.format(
+          DateTimeFormatter.ISO_OFFSET_DATE_TIME
+        )
+      )
+  }
+}
