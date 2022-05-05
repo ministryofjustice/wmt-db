0fe2699168ccb280bed5a518424100c6652406e8
carlov20 <carlov20@users.noreply.github.com>
2022-05-05 10:35:17 +0100

    WFP-1054 added new mapping for PO code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/mapper/DefaultGradeMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/mapper/DefaultGradeMapper.kt
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component
 
 @Component
 class DefaultGradeMapper : GradeMapper {
-  private val gradeMap: Map<String, String> = mapOf("PSQ" to "PSO", "PSP" to "PQiP", "PSM" to "PO", "PSC" to "SPO", "TPO" to "PQiP")
+  private val gradeMap: Map<String, String> = mapOf("PSQ" to "PSO", "PSP" to "PQiP", "PSM" to "PO", "PSC" to "SPO", "TPO" to "PQiP", "NPSM" to "PO")
 
   override fun deliusToStaffGrade(deliusCode: String?): String {
     return gradeMap[deliusCode] ?: "DMY"
