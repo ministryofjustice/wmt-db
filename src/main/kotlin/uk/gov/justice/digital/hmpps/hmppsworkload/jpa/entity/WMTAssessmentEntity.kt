05df89edf0d8c0e445712080108c890215a26e88
carlov20 <carlov20@users.noreply.github.com>
2022-06-22 11:25:48 +0100

    WFP-1126 removed redundant column mappings

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/entity/WMTAssessmentEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/entity/WMTAssessmentEntity.kt
@@ -15,67 +15,12 @@ data class WMTAssessmentEntity(
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   val id: Long? = null,
 
-  @Column
-  val assessmentDate: String? = null,
-
-  @Column
-  val assessmentCode: String? = null,
-
-  @Column(name = "assessment_desc")
-  val assessmentDescription: String? = null,
-
-  @Column(name = "assessment_staff_name")
-  val staffName: String? = null,
-
   @Column(name = "assessment_staff_key")
   val staffCode: String,
-
-  @Column(name = "assessment_staff_grade")
-  val staffGrade: String? = null,
 
   @Column(name = "assessment_team_key")
   val teamCode: String,
 
   @Column
-  val crn: String? = null,
-
-  @Column
-  val disposalOrReleaseDate: String? = null,
-
-  @Column
   val sentenceType: String,
-
-  @Column(name = "so_registration_date")
-  val registrationDate: String? = null,
-
-  @Column(name = "asmnt_outcome_cd")
-  val outcomeCode: String? = null,
-
-  @Column(name = "asmnt_outcome_desc")
-  val outcomeDescription: String? = null,
-
-  @Column(name = "last_saved_dt_referral_doc")
-  val lastSavedReferralDocumentDate: String? = null,
-
-  @Column(name = "last_saved_dt_assessment_doc")
-  val lastSavedAssessmentDocumentDate: String? = null,
-
-  @Column(name = "offender_manager_staff_name")
-  val personManagerName: String? = null,
-
-  @Column(name = "offender_manager_team_cd")
-  val personManagerTeamCode: String? = null,
-
-  @Column(name = "offender_manager_cluster_cd")
-  val personManagerClusterCode: String? = null,
-
-  @Column(name = "offender_manager_provider_cd")
-  val personManagerProviderCode: String? = null,
-
-  @Column
-  val completedDate: String? = null,
-
-  @Column(name = "offender_manager_pdu_cd")
-  val personManagerPduCode: String? = null,
-
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/entity/WMTCourtReportsEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/entity/WMTCourtReportsEntity.kt
@@ -15,61 +15,15 @@ data class WMTCourtReportsEntity(
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   val id: Long? = null,
 
-  @Column(name = "team_desc")
-  val teamName: String? = null,
-
   @Column
   val teamCode: String,
 
   @Column(name = "om_key")
   val staffCode: String,
 
-  @Column(name = "om_team_staff_grade")
-  val teamStaffGradeCode: String? = null,
-
   @Column(name = "sdr_last_30")
   val standardDeliveryReportCount: Int? = null,
 
-  @Column(name = "sdr_due_next_30")
-  val standardDeliveryReportsDueCount: Int? = null,
-
   @Column(name = "sdr_conv_last_30")
   val fastDeliveryReportCount: Int? = null,
-
-  @Column(name = "datestamp")
-  val effectiveFrom: String? = null,
-
-  @Column
-  val oralReports: String? = null,
-
-  @Column
-  val trust: String? = null,
-
-  @Column(name = "region_desc")
-  val regionName: String? = null,
-
-  @Column
-  val regionCode: String? = null,
-
-  @Column(name = "ldu_desc")
-  val lduName: String? = null,
-
-  @Column
-  val lduCode: String? = null,
-
-  @Column(name = "om_surname")
-  val staffSurname: String? = null,
-
-  @Column(name = "om_forename")
-  val staffForename: String? = null,
-
-  @Column(name = "om_grade_code")
-  val staffGradeCode: String? = null,
-
-  @Column
-  val pduCode: String? = null,
-
-  @Column(name = "pdu_desc")
-  val pduName: String? = null
-
 )
