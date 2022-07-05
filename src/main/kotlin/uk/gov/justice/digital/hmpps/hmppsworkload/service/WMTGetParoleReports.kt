52a62351478b0cc27762ac691aad6832e3fbfe6a
AndySweilam <AndySweilam@users.noreply.github.com>
2022-07-05 16:42:49 +0100

    WFP-1126 refactoring

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/WMTGetParoleReports.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/WMTGetParoleReports.kt
@@ -7,5 +7,5 @@ import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository.WMTInstitutiona
 class WMTGetParoleReports(private val wmtInstitutionalReportRepository: WMTInstitutionalReportRepository) :
   GetParoleReports {
   override fun getParoleReports(staffCode: String, teamCode: String): Int =
-    wmtInstitutionalReportRepository.findFirstByStaffCodeAndTeamCode(staffCode, teamCode)?.let { it.paroleReports } ?: 0
+    wmtInstitutionalReportRepository.findFirstByStaffCodeAndTeamCode(staffCode, teamCode)?.paroleReports ?: 0
 }
