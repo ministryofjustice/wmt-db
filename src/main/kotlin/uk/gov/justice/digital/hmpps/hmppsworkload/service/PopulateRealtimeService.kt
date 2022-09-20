c09c973a90e944f0a9e058c024cea028819c3ca1
carlov20 <carlov20@users.noreply.github.com>
2022-09-20 15:51:57 +0100

    WFP-821 added populate names endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/PopulateRealtimeService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/PopulateRealtimeService.kt
@@ -8,58 +8,45 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Mono
-import uk.gov.justice.digital.hmpps.hmppsworkload.client.CommunityApiClient
-import uk.gov.justice.digital.hmpps.hmppsworkload.controller.CaseCsv
+import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository.CaseDetailsRepository
 import uk.gov.justice.digital.hmpps.hmppsworkload.listener.HmppsOffenderEvent
 import uk.gov.justice.digital.hmpps.hmppsworkload.listener.SQSMessage
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.MissingQueueException
-import java.math.BigInteger
 
 @Service
 class PopulateRealtimeService(
   private val hmppsQueueService: HmppsQueueService,
   private val objectMapper: ObjectMapper,
   @Qualifier("hmppsoffenderqueue-sqs-client") private val hmppsOffenderSqsClient: AmazonSQSAsync,
-  @Qualifier("communityApiClient") private val communityApiClient: CommunityApiClient
+  private val caseDetailsRepository: CaseDetailsRepository
 ) {
 
   private val hmppsOffenderQueueUrl by lazy { hmppsQueueService.findByQueueId("hmppsoffenderqueue")?.queueUrl ?: throw MissingQueueException("HmppsQueue hmppsoffenderqueue not found") }
 
   @Async
-  fun sendEvents(cases: List<CaseCsv>) {
-    cases
-      .map { publishToHmppsOffenderQueue(it) }
-      .forEach {
-        it.onErrorResume { Mono.empty() }
-          .block()
-      }
+  fun populateEventsFromCaseDetails() {
+    caseDetailsRepository.findAll().forEach { caseDetailsEntity ->
+      publishToHmppsOffenderQueue(caseDetailsEntity.crn)
+    }
   }
 
-  private fun publishToHmppsOffenderQueue(case: CaseCsv): Mono<Any> {
-    return communityApiClient.getActiveConvictions(case.crn!!)
-      .map { convictions ->
-        convictions.filter { it.sentence != null }
-          .map { it.sentence!! }
-          .forEach {
-            val sendMessage = SendMessageRequest(
-              hmppsOffenderQueueUrl,
-              objectMapper.writeValueAsString(
-                caseToOffenderSqsMessage(case.crn!!, it.sentenceId)
-              )
-            ).withMessageAttributes(
-              mapOf("eventType" to MessageAttributeValue().withDataType("String").withStringValue("SENTENCE_CHANGED"))
-            )
-            log.info("publishing event type {} for crn {}", "SENTENCE_CHANGED", case.crn)
-            hmppsOffenderSqsClient.sendMessage(sendMessage)
-          }
-      }
+  private fun publishToHmppsOffenderQueue(crn: String) {
+    val sendMessage = SendMessageRequest(
+      hmppsOffenderQueueUrl,
+      objectMapper.writeValueAsString(
+        caseToOffenderSqsMessage(crn)
+      )
+    ).withMessageAttributes(
+      mapOf("eventType" to MessageAttributeValue().withDataType("String").withStringValue("OFFENDER_DETAILS_CHANGED"))
+    )
+    log.info("publishing event type {} for crn {}", "OFFENDER_DETAILS_CHANGED", crn)
+    hmppsOffenderSqsClient.sendMessage(sendMessage)
   }
 
-  private fun caseToOffenderSqsMessage(crn: String, sentenceId: BigInteger): SQSMessage = SQSMessage(
+  private fun caseToOffenderSqsMessage(crn: String): SQSMessage = SQSMessage(
     objectMapper.writeValueAsString(
-      HmppsOffenderEvent(crn, sentenceId)
+      HmppsOffenderEvent(crn, null)
     )
   )
 
