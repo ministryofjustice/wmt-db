69a3b1f5f551858082bd4476c59d9da0ad696c04
carlov20 <carlov20@users.noreply.github.com>
2022-09-21 11:10:01 +0100

    WFP-821 only populate when first name is empty

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/CaseDetailsRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/CaseDetailsRepository.kt
@@ -3,4 +3,6 @@ package uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository
 import org.springframework.data.repository.CrudRepository
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.CaseDetailsEntity
 
-interface CaseDetailsRepository : CrudRepository<CaseDetailsEntity, String>
+interface CaseDetailsRepository : CrudRepository<CaseDetailsEntity, String> {
+  fun findByFirstName(firstName: String): List<CaseDetailsEntity>
+}
