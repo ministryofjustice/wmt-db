9069aa401a32eb2baa92c20ea44b3664a2e5f34c
AndySweilam <AndySweilam@users.noreply.github.com>
2022-10-14 15:58:18 +0100

    WFP-1415: refactor reductions update with 1426 changes. Added tests.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/reduction/UpdateReductionService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/reduction/UpdateReductionService.kt
@@ -1,29 +1,21 @@
 package uk.gov.justice.digital.hmpps.hmppsworkload.service.reduction
 
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsworkload.domain.OutOfDateReductions
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.ReductionStatus
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository.ReductionsRepository
-import java.time.ZonedDateTime
 import javax.transaction.Transactional
 
 @Service
 class UpdateReductionService(private val reductionsRepository: ReductionsRepository) {
 
   @Transactional
-  fun updateReductionStatus() {
-    reductionsRepository.findAllByEffectiveFromBeforeAndEffectiveToBeforeAndStatus(
-      ZonedDateTime.now(),
-      ZonedDateTime.now(),
-      ReductionStatus.ACTIVE
-    )
-      .takeIf { it.isNotEmpty() }?.onEach { it.status = ReductionStatus.ARCHIVED }
-      ?.let { reductionsRepository.saveAll(it) }
+  fun updateReductionStatus(outOfDateReductions: OutOfDateReductions) {
+    outOfDateReductions.activeNowArchived
+      .onEach { it.status = ReductionStatus.ARCHIVED }
+      .let { reductionsRepository.saveAll(it) }
 
-    reductionsRepository.findAllByEffectiveFromBeforeAndEffectiveToAfterAndStatus(
-      ZonedDateTime.now(),
-      ZonedDateTime.now(),
-      ReductionStatus.SCHEDULED
-    )
+    outOfDateReductions.scheduledNowActive
       .takeIf { it.isNotEmpty() }?.onEach { it.status = ReductionStatus.ACTIVE }
       ?.let { reductionsRepository.saveAll(it) }
   }
