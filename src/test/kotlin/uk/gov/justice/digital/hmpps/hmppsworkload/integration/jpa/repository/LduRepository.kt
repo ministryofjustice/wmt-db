978514bd16c0f73712d3b66801ee45796102f1ef
carlov20 <carlov20@users.noreply.github.com>
2022-10-17 09:20:36 +0100

    represent WMT tables in JPA in tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/jpa/repository/LduRepository.kt
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsworkload.integration.jpa.repository
-
-import org.springframework.data.repository.CrudRepository
-import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.LduEntity
-
-interface LduRepository : CrudRepository<LduEntity, Long>
