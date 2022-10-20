787761df459e03e25f488ac06309179d6193939b
carlov20 <carlov20@users.noreply.github.com>
2022-10-20 09:01:46 +0100

    delete populate realtime code (#254)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/CaseDetailsRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/CaseDetailsRepository.kt
@@ -3,6 +3,4 @@ package uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository
 import org.springframework.data.repository.CrudRepository
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.CaseDetailsEntity
 
-interface CaseDetailsRepository : CrudRepository<CaseDetailsEntity, String> {
-  fun findByFirstName(firstName: String): List<CaseDetailsEntity>
-}
+interface CaseDetailsRepository : CrudRepository<CaseDetailsEntity, String>
