c86b4aac6c7b62bae78d6445b31b968dd89873ee
AndySweilam <AndySweilam@users.noreply.github.com>
2022-10-21 14:02:29 +0100

    WFP-1425: moved updateReductions tests to extract placed tests.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/listener/ExtractPlacedEventListenerTestTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/listener/ExtractPlacedEventListenerTestTest.kt
@@ -1,12 +1,21 @@
 package uk.gov.justice.digital.hmpps.hmppsworkload.integration.listener
 
 import com.amazonaws.services.sqs.model.SendMessageRequest
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.matches
+import org.awaitility.kotlin.untilCallTo
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.data.repository.findByIdOrNull
 import uk.gov.justice.digital.hmpps.hmppsworkload.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.hmppsworkload.integration.domain.WMTStaff
 import uk.gov.justice.digital.hmpps.hmppsworkload.integration.jpa.entity.ReductionCategoryEntity
 import uk.gov.justice.digital.hmpps.hmppsworkload.integration.jpa.entity.ReductionReasonEntity
+import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.ReductionEntity
+import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.ReductionStatus
+import java.math.BigDecimal
+import java.time.ZonedDateTime
 
 class ExtractPlacedEventListenerTestTest : IntegrationTestBase() {
 
@@ -27,5 +36,48 @@ class ExtractPlacedEventListenerTestTest : IntegrationTestBase() {
 
     noMessagesOnExtractPlacedQueue()
     noMessagesOnExtractPlacedDLQ()
+  }
+
+  @Test
+  fun `can update incorrect Active reduction to Archived`() {
+    val activeReductionWhichIsNowArchived = reductionsRepository.save(ReductionEntity(workloadOwner = wmtStaff.wmtWorkloadOwnerEntity, hours = BigDecimal.valueOf(3.2), effectiveFrom = ZonedDateTime.now().minusDays(2), effectiveTo = ZonedDateTime.now().minusDays(1), status = ReductionStatus.ACTIVE, reductionReasonId = reductionReason.id!!))
+    hmppsExtractPlacedClient.sendMessage(SendMessageRequest(hmppsExtractPlacedQueue.queueUrl, "{}"))
+
+    noMessagesOnExtractPlacedQueue()
+    Assertions.assertEquals(ReductionStatus.ARCHIVED, reductionsRepository.findByIdOrNull(activeReductionWhichIsNowArchived.id!!)?.status)
+  }
+
+  @Test
+  fun `can update incorrect Scheduled reduction to be Active`() {
+    val scheduledReductionWhichIsNowActive = reductionsRepository.save(ReductionEntity(workloadOwner = wmtStaff.wmtWorkloadOwnerEntity, hours = BigDecimal.valueOf(3.2), effectiveFrom = ZonedDateTime.now().minusDays(1), effectiveTo = ZonedDateTime.now().plusDays(1), status = ReductionStatus.SCHEDULED, reductionReasonId = reductionReason.id!!))
+    hmppsExtractPlacedClient.sendMessage(SendMessageRequest(hmppsExtractPlacedQueue.queueUrl, "{}"))
+    noMessagesOnExtractPlacedQueue()
+    Assertions.assertEquals(ReductionStatus.ACTIVE, reductionsRepository.findByIdOrNull(scheduledReductionWhichIsNowActive.id!!)?.status)
+  }
+
+  @Test
+  fun `do nothing when reduction status is correct`() {
+    val activeReduction = reductionsRepository.save(ReductionEntity(workloadOwner = wmtStaff.wmtWorkloadOwnerEntity, hours = BigDecimal.valueOf(3.2), effectiveFrom = ZonedDateTime.now().minusDays(1), effectiveTo = ZonedDateTime.now().plusDays(1), status = ReductionStatus.ACTIVE, reductionReasonId = reductionReason.id!!))
+    hmppsExtractPlacedClient.sendMessage(SendMessageRequest(hmppsExtractPlacedQueue.queueUrl, "{}"))
+    noMessagesOnExtractPlacedQueue()
+    Assertions.assertEquals(ReductionStatus.ACTIVE, reductionsRepository.findByIdOrNull(activeReduction.id!!)?.status)
+  }
+
+  @Test
+  fun `can publish out of date reductions when updating reduction status`() {
+    reductionsRepository.save(ReductionEntity(workloadOwner = wmtStaff.wmtWorkloadOwnerEntity, hours = BigDecimal.valueOf(3.2), effectiveFrom = ZonedDateTime.now().minusDays(1), effectiveTo = ZonedDateTime.now().plusDays(1), status = ReductionStatus.SCHEDULED, reductionReasonId = reductionReason.id!!))
+    hmppsExtractPlacedClient.sendMessage(SendMessageRequest(hmppsExtractPlacedQueue.queueUrl, "{}"))
+    noMessagesOnExtractPlacedQueue()
+    await untilCallTo { verifyReductionsCompletedOnQueue() } matches { it == true }
+  }
+
+  @Test
+  fun `out of date reductions message contain required data`() {
+    reductionsRepository.save(ReductionEntity(workloadOwner = wmtStaff.wmtWorkloadOwnerEntity, hours = BigDecimal.valueOf(3.2), effectiveFrom = ZonedDateTime.now().minusDays(1), effectiveTo = ZonedDateTime.now().plusDays(1), status = ReductionStatus.SCHEDULED, reductionReasonId = reductionReason.id!!))
+    hmppsExtractPlacedClient.sendMessage(SendMessageRequest(hmppsExtractPlacedQueue.queueUrl, "{}"))
+    noMessagesOnExtractPlacedQueue()
+    await untilCallTo { verifyReductionsCompletedOnQueue() } matches { it == true }
+
+    Assertions.assertEquals("OUT_OF_DATE_REDUCTIONS", getReductionsCompletedMessages().eventType)
   }
 }
