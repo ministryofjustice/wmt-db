24246102edc8572b1886a69dca48954a20061294
AndySweilam <AndySweilam@users.noreply.github.com>
2022-10-21 14:24:49 +0100

    WFP-1425: refactor updateOutOfDateReductions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/listener/ExtractPlacedEventListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/listener/ExtractPlacedEventListener.kt
@@ -2,17 +2,15 @@ package uk.gov.justice.digital.hmpps.hmppsworkload.listener
 
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.hmppsworkload.service.reduction.GetReductionService
 import uk.gov.justice.digital.hmpps.hmppsworkload.service.reduction.UpdateReductionService
 
 @Component
 class ExtractPlacedEventListener(
-  private val updateReductionService: UpdateReductionService,
-  private val getReductionService: GetReductionService
+  private val updateReductionService: UpdateReductionService
 ) {
 
   @JmsListener(destination = "hmppsextractplacedqueue", containerFactory = "hmppsQueueContainerFactoryProxy")
   fun processMessage(rawMessage: String) {
-    updateReductionService.updateReductionStatus(getReductionService.findOutOfDateReductions())
+    updateReductionService.updateOutOfDateReductionStatus()
   }
 }
