9a46cd0bd0860488e934316c632f2949f359249a
carlov20 <carlov20@users.noreply.github.com>
2022-10-28 10:57:51 +0100

    updated test setup

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/QueueAdminTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/QueueAdminTest.kt
@@ -8,7 +8,7 @@ class QueueAdminTest : IntegrationTestBase() {
   @Test
   fun `should not allow purge with no auth`() {
     webTestClient.put()
-      .uri("/queue-admin/purge-queue/hmpps_workload_s3_extract_event_dlq")
+      .uri("/queue-admin/purge-queue/hmpps_extract_placed_dlq")
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
       .expectStatus().isUnauthorized
@@ -17,7 +17,7 @@ class QueueAdminTest : IntegrationTestBase() {
   @Test
   fun `should allow purge with correct auth`() {
     webTestClient.put()
-      .uri("/queue-admin/purge-queue/hmpps_workload_s3_extract_event_dlq")
+      .uri("/queue-admin/purge-queue/hmpps_extract_placed_dlq")
       .headers { it.authToken(roles = listOf("ROLE_QUEUE_WORKLOAD_ADMIN")) }
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
