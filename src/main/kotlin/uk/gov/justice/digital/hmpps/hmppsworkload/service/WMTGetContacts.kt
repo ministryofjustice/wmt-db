c91b0eebf83b656e3111d9b044dcd8cea33f462c
carlov20 <carlov20@users.noreply.github.com>
2022-11-15 16:25:51 +0000

    removed GetContacts, updated to staff identifier (#292)
    * removed GetContacts, updated to staff identifier
    
    * fixed compile issue

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/WMTGetContacts.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/WMTGetContacts.kt
@@ -2,16 +2,17 @@ package uk.gov.justice.digital.hmpps.hmppsworkload.service
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsworkload.domain.Contact
+import uk.gov.justice.digital.hmpps.hmppsworkload.domain.StaffIdentifier
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository.WMTCMSRepository
 
 @Service
-class WMTGetContacts(private val wmtcmsRepository: WMTCMSRepository) : GetContacts {
+class WMTGetContacts(private val wmtcmsRepository: WMTCMSRepository) {
 
-  override fun findContactsOutsideCaseload(staffCode: String, teamCode: String): List<Contact> =
-    wmtcmsRepository.findByStaffTeamCodeAndStaffCodeAndPersonManagerStaffCodeNot(teamCode, staffCode, staffCode)
+  fun findContactsOutsideCaseload(staffIdentifier: StaffIdentifier): List<Contact> =
+    wmtcmsRepository.findByStaffTeamCodeAndStaffCodeAndPersonManagerStaffCodeNot(staffIdentifier.teamCode, staffIdentifier.staffCode, staffIdentifier.staffCode)
       .map { Contact(it.contactTypeCode) }
 
-  override fun findContactsInCaseloadPerformedByOthers(staffCode: String, teamCode: String): List<Contact> =
-    wmtcmsRepository.findByPersonManagerTeamCodeAndPersonManagerStaffCodeAndStaffCodeNot(teamCode, staffCode, staffCode)
+  fun findContactsInCaseloadPerformedByOthers(staffIdentifier: StaffIdentifier): List<Contact> =
+    wmtcmsRepository.findByPersonManagerTeamCodeAndPersonManagerStaffCodeAndStaffCodeNot(staffIdentifier.teamCode, staffIdentifier.staffCode, staffIdentifier.staffCode)
       .map { Contact(it.contactTypeCode) }
 }
