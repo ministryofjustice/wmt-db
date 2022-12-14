2e9d6f76ac08566447e38354cd279e9b6cc68fb1
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-12-14 08:19:22 +0000

    FIX: remove staffId from choose-practitioner endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/client/dto/TeamStaff.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/client/dto/TeamStaff.kt
@@ -2,10 +2,8 @@ package uk.gov.justice.digital.hmpps.hmppsworkload.client.dto
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import uk.gov.justice.digital.hmpps.hmppsworkload.mapper.deliusToStaffGrade
-import java.math.BigInteger
 
 data class TeamStaff @JsonCreator constructor(
-  val staffIdentifier: BigInteger,
   val staffCode: String,
   val staff: StaffName,
   val staffGrade: StaffGrade?,
