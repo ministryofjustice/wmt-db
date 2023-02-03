9d084100b0be8462e3f7ca6314b7e46f02f9fbc4
carlov20 <carlov20@users.noreply.github.com>
2023-02-03 08:53:24 +0000

    WFP-1701 removed unused fields

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/domain/OffenderManagerPotentialWorkload.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/domain/OffenderManagerPotentialWorkload.kt
@@ -9,16 +9,8 @@ import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.mapping.OffenderManagerOve
 import java.math.BigDecimal
 
 data class OffenderManagerPotentialWorkload @JsonCreator constructor(
-  @Schema(description = "Probation Practitioner forename", example = "John")
-  val forename: String,
-  @Schema(description = "Probation Practitioner surname", example = "Smith")
-  val surname: String,
-  @Schema(description = "Probation Practitioner Grade", example = "PO")
-  val grade: String,
   @Schema(description = "Probation Practitioner capacity correct to one decimal place", example = "56.7")
   val capacity: BigDecimal,
-  @Schema(description = "Offender Manager Code", example = "OM1")
-  val code: String,
   @Schema(description = "Probation Practitioner potential capacity after case is allocated", example = "8.2")
   val potentialCapacity: BigDecimal,
   @Schema(description = "Name of Person on Probation")
@@ -30,11 +22,7 @@ data class OffenderManagerPotentialWorkload @JsonCreator constructor(
   companion object {
     fun from(offenderManagerOverview: OffenderManagerOverview, impactResponse: ImpactResponse, potentialCase: Case): OffenderManagerPotentialWorkload {
       return OffenderManagerPotentialWorkload(
-        impactResponse.staff.name.forename,
-        impactResponse.staff.name.surname,
-        impactResponse.staff.getGrade(),
         offenderManagerOverview.capacity,
-        impactResponse.staff.code,
         offenderManagerOverview.potentialCapacity!!,
         impactResponse.name,
         impactResponse.staff,
