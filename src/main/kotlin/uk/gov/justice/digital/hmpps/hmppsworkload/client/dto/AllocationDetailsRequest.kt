d6a0645316e4d42d4fabf4a6de64ae69059255b3
sp-watson <steve.watson@digital,justice.gov.uk>
2023-03-17 09:02:27 +0000

    WFP-1658: Fix json serialisation of allocation details post body
    I believe the root cause of this issue is that the object mapper does not support all kotlin features out of the box - jackson provides a kotlin library for this reason - and one of those missing features is private field serialisation in data classes.
    
    Rather than spending time trying to get this wired in to spring I have just fixed this issue

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/client/dto/AllocationDetailsRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/client/dto/AllocationDetailsRequest.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsworkload.client.dto
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.EventManagerEntity
 
 data class AllocationDetailsRequest(
-  private val cases: List<AllocationDetailCase>
+  val cases: List<AllocationDetailCase>
 ) {
   companion object {
     fun from(eventManagers: List<EventManagerEntity>): AllocationDetailsRequest =
