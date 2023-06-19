8ab4c022347b99215ac5ed9ae24d5a4087d92ac5
Andy Sweilam <104897795+AndySweilam@users.noreply.github.com>
2023-06-19 11:10:03 +0100

    Wfp 2014 (#463)
    * WFP-2014: refactor jwt to avoid using deprecated library.
    
    * WFP-2014: refactor createJwt signWith to use non-deprecated library.
    
    * WFP-2014: increase constructors parameters in KLint.
    
    * WFP-2014: reduce breakdown workload calculation function.
    
    * WFP-2014: refactor workload calculation function.
    
    ---------
    
    Co-authored-by: AndySweilam <AndySweilam@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/WorkloadCalculationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/WorkloadCalculationService.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsworkload.service
 
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsworkload.domain.Case
 import uk.gov.justice.digital.hmpps.hmppsworkload.domain.Contact
 import uk.gov.justice.digital.hmpps.hmppsworkload.domain.CourtReport
 import uk.gov.justice.digital.hmpps.hmppsworkload.domain.CourtReportType
@@ -12,6 +13,7 @@ import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository.WorkloadCalcula
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository.WorkloadPointsRepository
 import uk.gov.justice.digital.hmpps.hmppsworkload.service.reduction.GetReductionService
 import java.math.BigDecimal
+import java.math.BigInteger
 
 @Service
 class WorkloadCalculationService(
@@ -34,7 +36,7 @@ class WorkloadCalculationService(
       staffGrade,
     ),
   ): WorkloadCalculationEntity {
-    return workloadCalculationRepository.save(calculate(staffIdentifier.staffCode, staffIdentifier.teamCode, staffGrade, availableHours))
+    return workloadCalculationRepository.save(calculate(staffIdentifier, staffGrade, availableHours))
   }
 
   private fun getAvailableHours(
@@ -46,45 +48,63 @@ class WorkloadCalculationService(
     )
   }
 
-  private fun calculate(staffCode: String, teamCode: String, staffGrade: String, availableHours: BigDecimal): WorkloadCalculationEntity {
-    val staffIdentifier = StaffIdentifier(staffCode, teamCode)
-    val cases = getCaseLoad.getCases(staffIdentifier)
-    val courtReports = getCourtReports.getCourtReports(staffIdentifier)
-    val contactsPerformedOutsideCaseload = getContacts.findContactsOutsideCaseload(staffIdentifier)
-    val contactsPerformedByOthers = getContacts.findContactsInCaseloadPerformedByOthers(staffIdentifier)
-    val contactTypeWeightings = getContactTypeWeightings.findAll()
-    val t2aWorkloadPoints = workloadPointsRepository.findFirstByIsT2AAndEffectiveToIsNullOrderByEffectiveFromDesc(true)
-    val workloadPointsWeighting: WorkloadPointsEntity = workloadPointsRepository.findFirstByIsT2AAndEffectiveToIsNullOrderByEffectiveFromDesc(false)
-    val availablePoints = calculateAvailablePoints(workloadPointsWeighting.getDefaultPointsAvailable(staffGrade), workloadPointsWeighting.getDefaultContractedHours(staffGrade), availableHours)
-    val workloadPoints = workloadCalculator.getWorkloadPoints(
-      cases,
-      courtReports,
-      contactsPerformedOutsideCaseload,
-      contactsPerformedByOthers,
-      contactTypeWeightings,
-      t2aWorkloadPoints,
-      workloadPointsWeighting,
-    )
+  private fun calculate(staffIdentifier: StaffIdentifier, staffGrade: String, availableHours: BigDecimal): WorkloadCalculationEntity {
+    val workloadCalculationsElements = getWorkloadCalculationElements(staffIdentifier)
+
     return WorkloadCalculationEntity(
-      availablePoints = availablePoints,
-      workloadPoints = workloadPoints,
+      availablePoints = calculateAvailablePoints(
+        workloadCalculationsElements.workloadPointsElements.workloadPointsWeighting.getDefaultPointsAvailable(staffGrade),
+        workloadCalculationsElements.workloadPointsElements.workloadPointsWeighting.getDefaultContractedHours(staffGrade),
+        availableHours,
+      ),
+      workloadPoints = workloadCalculationsElements.workloadPoints,
       staffCode = staffIdentifier.staffCode,
       teamCode = staffIdentifier.teamCode,
       breakdownData = BreakdownDataEntity(
-        getCourtReportCounts(courtReports, CourtReportType.STANDARD),
-        getCourtReportCounts(courtReports, CourtReportType.FAST),
-        getContactTypeCodeCounts(contactsPerformedOutsideCaseload),
-        getContactTypeCodeCounts(contactsPerformedByOthers),
-        contactTypeWeightings,
-        cases.size,
+        getCourtReportCounts(workloadCalculationsElements.workloadPointsElements.courtReports, CourtReportType.STANDARD),
+        getCourtReportCounts(workloadCalculationsElements.workloadPointsElements.courtReports, CourtReportType.FAST),
+        getContactTypeCodeCounts(workloadCalculationsElements.workloadPointsElements.contactsPerformedOutsideCaseload),
+        getContactTypeCodeCounts(workloadCalculationsElements.workloadPointsElements.contactsPerformedByOthers),
+        workloadCalculationsElements.workloadPointsElements.contactTypeWeightings,
+        workloadCalculationsElements.workloadPointsElements.cases.size,
         availableHours,
       ),
     )
   }
 
+  private fun getWorkloadCalculationElements(staffIdentifier: StaffIdentifier): WorkloadCalculationElements {
+    val workloadPointsElements = WorkloadPointsElements(
+      getCaseLoad.getCases(staffIdentifier),
+      getCourtReports.getCourtReports(staffIdentifier),
+      getContacts.findContactsOutsideCaseload(staffIdentifier),
+      getContacts.findContactsInCaseloadPerformedByOthers(staffIdentifier),
+      getContactTypeWeightings.findAll(),
+      workloadPointsRepository.findFirstByIsT2AAndEffectiveToIsNullOrderByEffectiveFromDesc(true),
+      workloadPointsRepository.findFirstByIsT2AAndEffectiveToIsNullOrderByEffectiveFromDesc(false),
+    )
+    return WorkloadCalculationElements(
+      workloadPointsElements,
+      workloadCalculator.getWorkloadPoints(workloadPointsElements),
+    )
+  }
   private fun getCourtReportCounts(courtReports: List<CourtReport>, type: CourtReportType): Int =
     courtReports.count { it.type == type }
 
   private fun getContactTypeCodeCounts(contacts: List<Contact>): Map<String, Int> =
     contacts.groupingBy { it.typeCode }.eachCount()
 }
+
+data class WorkloadCalculationElements(
+  val workloadPointsElements: WorkloadPointsElements,
+  val workloadPoints: BigInteger,
+)
+
+data class WorkloadPointsElements(
+  val cases: List<Case>,
+  val courtReports: List<CourtReport>,
+  val contactsPerformedOutsideCaseload: List<Contact>,
+  val contactsPerformedByOthers: List<Contact>,
+  val contactTypeWeightings: Map<String, Int>,
+  val t2aWorkloadPoints: WorkloadPointsEntity,
+  val workloadPointsWeighting: WorkloadPointsEntity,
+)
