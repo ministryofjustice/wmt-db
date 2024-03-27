a652133ff03d55964fd005bbdbab4aa4874f3111
Gary Reid <gary.reid@digital.justice.gov.uk>
2024-03-27 14:48:17 +0000

    WFP-2345 revert new columns added for WFP-2345

---
+++ src/main/resources/db/migration/V1_60__PROBATION_RESET_DELETE_NEW_COLUMNS_APP.sql
@@ -0,0 +1,52 @@
+/* workload_points */
+
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_1_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_2_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_3_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_4_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_5_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_6_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_7_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_8_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_9_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_10_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_11_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_12_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_13_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_14_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_15_s;
+ALTER TABLE app.workload_points DROP COLUMN comm_tier_16_s;
+
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_1_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_2_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_3_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_4_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_5_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_6_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_7_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_8_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_9_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_10_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_11_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_12_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_13_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_14_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_15_s;
+ALTER TABLE app.workload_points DROP COLUMN cust_tier_16_s;
+
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_1_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_2_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_3_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_4_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_5_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_6_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_7_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_8_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_9_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_10_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_11_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_12_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_13_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_14_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_15_s;
+ALTER TABLE app.workload_points DROP COLUMN lic_tier_16_s;
