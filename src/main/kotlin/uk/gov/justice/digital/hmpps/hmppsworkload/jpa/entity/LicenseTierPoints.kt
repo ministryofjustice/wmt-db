76ff2640f23b81510a0d7f522418b43ff74c0f33
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-04-08 16:12:50 +0100

    WFP-2455-adding in tiers to jpa entities

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/entity/LicenseTierPoints.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/entity/LicenseTierPoints.kt
@@ -39,6 +39,38 @@ data class LicenseTierPoints(
   val D1Points: BigInteger,
   @Column(name = "lic_tier_16")
   val D0Points: BigInteger,
+  @Column(name = "lic_tier_1_s")
+  val A3SPoints: BigInteger,
+  @Column(name = "lic_tier_2_s")
+  val A2SPoints: BigInteger,
+  @Column(name = "lic_tier_3_s")
+  val A1SPoints: BigInteger,
+  @Column(name = "lic_tier_4_s")
+  val A0SPoints: BigInteger,
+  @Column(name = "lic_tier_5_s")
+  val B3SPoints: BigInteger,
+  @Column(name = "lic_tier_6_s")
+  val B2SPoints: BigInteger,
+  @Column(name = "lic_tier_7_s")
+  val B1SPoints: BigInteger,
+  @Column(name = "lic_tier_8_s")
+  val B0SPoints: BigInteger,
+  @Column(name = "lic_tier_9_s")
+  val C3SPoints: BigInteger,
+  @Column(name = "lic_tier_10_s")
+  val C2SPoints: BigInteger,
+  @Column(name = "lic_tier_11_s")
+  val C1SPoints: BigInteger,
+  @Column(name = "lic_tier_12_s")
+  val C0SPoints: BigInteger,
+  @Column(name = "lic_tier_13_s")
+  val D3SPoints: BigInteger,
+  @Column(name = "lic_tier_14_s")
+  val D2SPoints: BigInteger,
+  @Column(name = "lic_tier_15_s")
+  val D1SPoints: BigInteger,
+  @Column(name = "lic_tier_16_s")
+  val D0SPoints: BigInteger,
 ) {
   fun asMap(): Map<Tier, BigInteger> {
     return mapOf(
@@ -58,6 +90,22 @@ data class LicenseTierPoints(
       Tier.D2 to D2Points,
       Tier.D1 to D1Points,
       Tier.D0 to D0Points,
+      Tier.A3S to A3SPoints,
+      Tier.A2S to A2SPoints,
+      Tier.A1S to A1SPoints,
+      Tier.A0S to A0SPoints,
+      Tier.B3S to B3SPoints,
+      Tier.B2S to B2SPoints,
+      Tier.B1S to B1SPoints,
+      Tier.B0S to B0SPoints,
+      Tier.C3S to C3SPoints,
+      Tier.C2S to C2SPoints,
+      Tier.C1S to C1SPoints,
+      Tier.C0S to C0SPoints,
+      Tier.D3S to D3SPoints,
+      Tier.D2S to D2SPoints,
+      Tier.D1S to D1SPoints,
+      Tier.D0S to D0SPoints,
     )
   }
 }
