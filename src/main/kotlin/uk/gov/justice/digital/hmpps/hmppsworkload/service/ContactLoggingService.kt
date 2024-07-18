f2311ffb52e72297bfad6210d344e01879e8b2c5
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-07-18 14:46:20 +0100

    WFP-2619 tidy up constants

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/ContactLoggingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/service/ContactLoggingService.kt
@@ -6,18 +6,22 @@ import org.slf4j.MDC
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsworkload.domain.event.ContactLoggingMessage
 
+private const val CRN = "CRN"
+private const val EDIT_NOTES_SCREEN_ACCESSED = "EDIT_NOTES_SCREEN_ACCESSED"
+private const val NOTES_EDITED = "NOTES_EDITED"
+
 @Service
 class ContactLoggingService {
   val log = LoggerFactory.getLogger(this::class.java)
   suspend fun logContact(message: ContactLoggingMessage): Boolean {
     try {
-      MDC.put("CRN", message.crn)
-      MDC.put("EDIT_NOTES_SCREEN_ACCESSED", message.editNotesScreenAccessed.toString())
-      MDC.put("NOTES_EDITED", message.notesEdited.toString())
+      MDC.put(CRN, message.crn)
+      MDC.put(EDIT_NOTES_SCREEN_ACCESSED, message.editNotesScreenAccessed.toString())
+      MDC.put(NOTES_EDITED, message.notesEdited.toString())
       log.info("CRN: ${message.crn} allocated, EDIT_NOTES_SCREEN_ACCESSED: ${message.editNotesScreenAccessed}, NOTES_EDITED: ${message.notesEdited}")
-      MDC.remove("CRN")
-      MDC.remove("EDIT_NOTES_SCREEN_ACCESSED")
-      MDC.remove("NOTES_EDITED")
+      MDC.remove(CRN)
+      MDC.remove(EDIT_NOTES_SCREEN_ACCESSED)
+      MDC.remove(NOTES_EDITED)
       return true
     } catch (e: LoggingException) {
       log.error(e.message, e)
