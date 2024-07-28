c07bde893ea71251346963ecd14cd4c6b2e32487
Gary Reid <gary.reid@digital.justice.gov.uk>
2024-07-28 18:53:15 +0100

    WFP-2572 lint error

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/request/AllocateCase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/request/AllocateCase.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsworkload.integration.request
 
-fun allocateCase(crn: String, eventNumber: Int, sendEmailCopyToAllocatingOfficer: Boolean = true, spoOversightNotes:String = "Overs notes", sensitiveOversightNotes:Boolean = false) = """
+fun allocateCase(crn: String, eventNumber: Int, sendEmailCopyToAllocatingOfficer: Boolean = true, spoOversightNotes: String = "Overs notes", sensitiveOversightNotes: Boolean = false) = """
   {
      "crn": "$crn",
      "emailTo" : ["additionalEmailReceiver@test.justice.gov.uk"],
