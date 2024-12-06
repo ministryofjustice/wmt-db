68d26a3e68fc7bf86bef760d0991cd551f3c2a75
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-12-06 12:27:31 +0000

    WFP-2884 - unused import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/controller/EventManagerController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/controller/EventManagerController.kt
@@ -7,7 +7,6 @@ import jakarta.persistence.EntityNotFoundException
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
