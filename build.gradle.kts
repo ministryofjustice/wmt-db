a6560094e309bd57b654a90e28bdb01ee77439ba
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-12-10 10:14:07 +0000

    updating gradle spring boot to 6.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.7"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.0"
   kotlin("plugin.spring") version "2.0.21"
   kotlin("plugin.jpa") version "2.0.21"
   id("io.gitlab.arturbosch.detekt").version("1.23.7")
